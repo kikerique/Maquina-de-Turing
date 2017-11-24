30d2c3f8d19ec24e5fcd995400b98ee2e4827741
Enrique <enrique-romero_98@hotmail.com>
2017-11-23 20:10:56 -0600

    Pues a ver si no la cague en lo que llevo de multiplicacion

--- mainTuring.c
+++ mainTuring.c
@@ -6,6 +6,7 @@ void manejaError(int);
 void pideCadena(char*);
 void pasaLista(LISTA,LISTA,char*);//Pasa los numeros de formato a#b a Lista1:a y Lista2:b
 void hazMultiplicacion(LISTA,LISTA);
+void enviaAlFinal(LISTA);
 
 int main()
 {
@@ -29,23 +30,104 @@ void pasaLista(LISTA list1, LISTA list2, char* cadena)
 	while(cadena[i]!='#')
 	{
 		numero=(int)cadena[i] - (int)'0';
-		lista1=insertar(lista1,numero);//agregar a list 1
-		i++;
+		if(numero=>0 || numero<=1)
+		{
+			list1=insertar(list1,numero);//agregar a list 1
+			i++;
+		}else{
+			printf("\nNumero rechazado\n");
+			break;
+		}
+		
 	}
 	j=i+1;
 	while(cadena[j]!='\0')
 	{
-		lista2=insertar(lista2,numero);//agregar a list 2
-		j++;
+		numero=(int)cadena[i] - (int)'0';
+		if(numero=>0 || numero<=1)
+		{
+			list2=insertar(list2,numero);//agregar a list 1
+			i++;
+		}else{
+			printf("\nNumero rechazado\n");
+			break;
+		}
 	}
 }
 void hazMultiplicacion(LISTA list1,LISTA list2)
+{	
+	LISTA resultado=creaLista(),aux=resultado,aux1=list1,aux2=list2;//Resultado guarda la lista de resultado y aux(n) se usa para moverse dentro de las listas creadas
+	int multiplicacion=0,restante=0, intento=0;//el primer intento solo guarda el resultado de multiplicar el ultimo nodo de list1 con todos los nodos de list2
+	enviaAlFinal(aux1);
+	enviaAlFinal(aux2);
+	while(esVaciaLista(aux1)!=1)
+	{
+		while(esVaciaLista(aux2)!=1)//la list2 se recorre de izquierda a derecha 
+		{	
+				if(intento==0)
+				{
+					multiplicacion=(aux1->dato)*(aux2->dato);
+					aux=insertarInicio(resultado,multiplicacion);//los numeros se recorren de derecha a izquierda, por eso el insertar al inicio
+					aux2=aux2->ant;
+				}else{
+					multiplicacion=(aux1->dato)*(aux2->dato);
+					if(esVaciaLista(aux)!=1)//el resultado se recorre paralelo a la list2 pero hay veces que sobra un byte de carry
+					{
+						if(multiplicacion+(aux->dato)+restante==3)
+						{
+							aux->dato=0;
+							if((aux->ant)!=NULL)
+							{
+								(aux->ant)->dato=(resultado,1);
+								
+							}else{
+								aux=insertarInicio(resultado,multiplicacion+restante);//el byte de carry se inserta al inicio de resultado
+								
+							}
+							restante=1;//byte de carry
+						}else{
+							if(multiplicacion+(aux->dato)+restante==2)
+							{
+								aux->dato=0;
+								if((aux->ant)!=NULL)
+								{	
+									(aux->ant)->dato=(resultado,1);
+									
+								}else{
+									aux=insertarInicio(resultado,multiplicacion+restante);//el byte de carry se inserta al inicio de resultado
+								}
+								restante=0;
+							}else{
+								aux->dato=multiplicacion+(aux->dato)+restante;
+								restante=0;
+							}
+						}
+						
+					}else{
+						aux=insertarInicio(resultado,multiplicacion+restante);//el byte de carry se inserta al inicio de resultado
+						restante=0;
+					}
+					aux=aux->ant;     //Resultado y lista dos se recorren paralelamente
+					aux2=aux2->ant;
+				}
+			
+		}
+		insertar(aux2,0);
+		enviaAlFinal(aux2);
+		enviaAlFinal(aux);
+		aux1=aux1->ant;
+		intento++;
+		restante=0;
+	}
+	
+}
+void enviaAlFinal(LISTA list)
 {
-	int i=0,j=0;
-	while(/*Lista 1 llegue al fin*/){ i++;}
-	
-	while(/*Lista 2 llegue al fin*/){j++;}
-	
+	while(esVaciaLista(list)!=1)//envia la lista al final
+	{
+		list=list->sig;
+	}
+	list=list->ant;
 }
 void manejaError(int e)
 {
